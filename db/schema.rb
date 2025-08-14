49a1ff73ebed5944f6e46b50e3e249c8df4826fe
EC2 Default User <ec2-user@ip-172-31-38-20.ap-northeast-1.compute.internal>
2025-08-14 13:39:01 +0000

    [Add]if user_signed_in?

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_08_12_110923) do
+ActiveRecord::Schema.define(version: 2025_08_14_124345) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -44,6 +44,11 @@ ActiveRecord::Schema.define(version: 2025_08_12_110923) do
     t.string "title"
     t.text "body"
     t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "post_books", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
