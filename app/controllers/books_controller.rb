fa79484d0574b488502b8635286895082a6cf476
daichi sakai <daichi.fkcx2347@gmail.com>
2025-08-22 14:50:42 +0000

    [Add]user info

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,11 +18,18 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = current_user
   end
 
   def edit
   end
 
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+  
   private
   def book_params
     params.require(:book).permit(:title, :body)
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'homes/about' => "homes#about", as: 'about'
-  resources :books, only: [:new, :index, :show, :edit, :create, :about]
+  resources :books, only: [:new, :index, :show, :edit, :create, :destroy, :about]
   resources :users, only: [:show, :edit, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
