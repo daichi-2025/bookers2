1d51fd11a1d27feca60b84cdc6ab68bd3edeb00b
daichi sakai <daichi.fkcx2347@gmail.com>
2025-08-25 08:33:46 +0000

    [Add] user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,33 @@
 class UsersController < ApplicationController
   def show
     @user = current_user
-    @books = Book.all
+    @books = @user.books.all
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.user_id = current_user.id
+    book.save
+    redirect_to books_path
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user.id)
   end
 
   def new
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'homes/about' => "homes#about", as: 'about'
   resources :books, only: [:new, :index, :show, :edit, :create, :destroy, :about]
-  resources :users, only: [:show, :edit, :index]
+  resources :users, only: [:show, :edit, :index, :update]
   patch 'books/:id' => 'books#update', as: 'update_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
